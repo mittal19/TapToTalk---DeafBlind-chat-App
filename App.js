bd93decd5e55a320fdb43d84c70c19d389fed0f8
Priyanshu Mittal <mittalpriyanshu19@gmail.com>
2021-04-13 18:08:22 +0530

    Added splash screen 1st version

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import {View,Text} from 'react-native';
+import {View,Text,StatusBar} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
 function App() 
 {
     useEffect(() => 
     {    
-        SplashScreen.hide();
+        console.log("in1");
+        setTimeout(async()=>
+        {
+            console.log("in2");
+            SplashScreen.hide();
+        },100000);
+        console.log("in3");
     });
 
-    return(<View><Text>fs</Text></View>)
+    return(
+        <View>
+            <StatusBar 
+                backgroundColor="#292F3F"
+                barStyle="light-content"
+            />
+            <Text>fs</Text>
+        </View>
+    );
 }
 export default App;
