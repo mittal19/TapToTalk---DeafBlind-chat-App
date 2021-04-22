28be01d5a7ea607e76ba5c6ed1a8825c209c8022
Priyanshu Mittal <mittalpriyanshu19@gmail.com>
2021-04-22 22:19:24 +0530

    Added some responsive code, alter screen on keyboard show and some others

--- components/component_userDetails.js
+++ components/component_userDetails.js
@@ -1,8 +1,8 @@
 //Here usesr details are taken and then stored in local storage
 //Then logIn Memo function created in App.js is called with the details passed.
 
-import React from 'react';
-import {View,Text,TextInput,TouchableOpacity,ToastAndroid,Modal,Pressable} from 'react-native';
+import React,{useEffect} from 'react';
+import {View,Text,TextInput,TouchableOpacity,ToastAndroid,Modal,Pressable,Image,Keyboard} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {AuthContext} from '../helpers/context';
 import styles,{width,height} from './styling/style_userDetails';
@@ -18,11 +18,30 @@ export function component_userDetails({route,navigation})
     const {userNumber} = route.params;        //getting info from previous screen
      
     const [userName,set_userName] = React.useState('');         //will hold name
-    const [userState,set_userState] = React.useState('');    //will hold status
-    const [userProfile,set_userProfile] = React.useState('');
-    const [userProfileName,set_userProfileName] = React.useState('');
+    const [userState,set_userState] = React.useState('notImpaired');    //will hold status
+    const [userProfile,set_userProfile] = React.useState('default');
+    const [userProfileName,set_userProfileName] = React.useState('defaultProfile.png');
     const [selectedOption, set_selectedOption] =   React.useState();
     const [modalVisible, setModalVisible] = React.useState(false);
+
+    useEffect(() => 
+    {
+        Keyboard.addListener("keyboardDidShow", _keyboardDidShow);
+        Keyboard.addListener("keyboardDidHide", _keyboardDidHide);
+
+        // cleanup function
+        return () => 
+        {
+        Keyboard.removeListener("keyboardDidShow", _keyboardDidShow);
+        Keyboard.removeListener("keyboardDidHide", _keyboardDidHide);
+        };
+    }, []);
+
+    const [keyboardStatus, setKeyboardStatus] = React.useState(false);
+
+    const _keyboardDidShow = () => setKeyboardStatus(true);
+
+    const _keyboardDidHide = () => setKeyboardStatus(false);
 
     const function_submitDetails =async()=>             //this function will be called when user clicks submit details button
     { 
@@ -32,82 +51,107 @@ export function component_userDetails({route,navigation})
             {
                 ToastAndroid.show("Enter all details",ToastAndroid.SHORT);               //if not show error
             }
-            else if(userProfileName.length<1||userProfile.length<1)
-            {
-                ToastAndroid.show("Some error while selecting Profile! Try again.",ToastAndroid.SHORT);
-            }
             else     //if all details are entered
             {  
                 try
                 {
-                    const resp = await storage().ref(userProfileName).putFile(userProfile);
-                    console.log(resp);
-                    if(resp["state"]=="success")
-                    { 
+                    var resp=null;
+                    if(userProfile!='default')
+                    {
+                        resp = await storage().ref(userProfileName).putFile(userProfile);
+                        console.log(resp);
+                        if(resp["state"]=="success")
+                        { 
+                            await AsyncStorage.setItem('userNumber',userNumber);            //set all details in local storage of phone
+                            await AsyncStorage.setItem('userName',userName);
+                            await AsyncStorage.setItem('userState',userState);
+                            await AsyncStorage.setItem('userProfile',userProfile);
+                            logIn(userNumber,userName,userState,userProfile);           //call function created in App.js using memo
+                        }
+                        else
+                        {   
+                            ToastAndroid.show("Some error occured.Try again!",ToastAndroid.SHORT);
+                        }
+                    }
+                    else
+                    {
                         await AsyncStorage.setItem('userNumber',userNumber);            //set all details in local storage of phone
                         await AsyncStorage.setItem('userName',userName);
                         await AsyncStorage.setItem('userState',userState);
                         await AsyncStorage.setItem('userProfile',userProfile);
                         logIn(userNumber,userName,userState,userProfile);           //call function created in App.js using memo
                     }
-                    else
-                    {   
-                        ToastAndroid.show("Some error occured.Try again!",ToastAndroid.SHORT);
-                    }
+                    
                 }
                 catch(err)
                 {
+                    console.log(err);
                     ToastAndroid.show("Some error occured.Try again!",ToastAndroid.SHORT);
                 }
             }
         }
         catch(e)
         {
+            console.log(e);
             ToastAndroid.show("Some error occurred! Try again",ToastAndroid.SHORT);
             navigation.pop();
         }
     }
 
-
     const function_openGallery=async()=>
     {
-        await ImagePicker.openPicker({
-            width:500,
-            height:300,
-            cropping:true
-        }).then(image =>
-            {
-                var uri = image["path"];
-                var nameoffile = uri.substring(uri.lastIndexOf('/')+1);
-                console.log(uri);
-                console.log(nameoffile);
-                set_userProfile(uri);
-                set_userProfileName(nameoffile);
-                //navigation.navigate('Save Profile',{userNumber,userName,userStatus,userProfile,uri,nameoffile});
-            });
-        setModalVisible(!modalVisible)
+        try
+        {
+            setModalVisible(!modalVisible);
+            await ImagePicker.openPicker({
+                width:500,
+                height:500,
+                cropping:true
+            }).then(image =>
+                {
+                    var uri = image["path"];
+                    var nameoffile = uri.substring(uri.lastIndexOf('/')+1);
+                    console.log(uri);
+                    console.log(nameoffile);
+                    set_userProfile(uri);
+                    set_userProfileName(nameoffile);
+                    //navigation.navigate('Save Profile',{userNumber,userName,userStatus,userProfile,uri,nameoffile});
+                });
+            
+        }
+        catch(err)
+        {
+            console.log(err);
+        }
     }
 
     const function_openCamera=async()=>
     {
-        await ImagePicker.openCamera({
-            widht:500,
-            height:500,
-            cropping:true
-        }).then(image =>
-            {
-                var uri = image["path"];
-                var nameoffile = uri.substring(uri.lastIndexOf('/')+1);
-                console.log(uri);
-                console.log(nameoffile);
-                set_userProfile(uri);
-                set_userProfileName(nameoffile);
-            });
-        setModalVisible(!modalVisible)
+        try
+        {
+            setModalVisible(!modalVisible);
+            await ImagePicker.openCamera({
+                widht:500,
+                height:500,
+                cropping:true
+            }).then(image =>
+                {
+                    var uri = image["path"];
+                    var nameoffile = uri.substring(uri.lastIndexOf('/')+1);
+                    console.log(uri);
+                    console.log(nameoffile);
+                    set_userProfile(uri);
+                    set_userProfileName(nameoffile);
+                });
+        }
+        catch(err)
+        {
+            console.log(err);
+        }
     }
 
     return(
-        <View style={{flex:1,backgroundColor:'#3E4DC8'}}>
+        <View style={styles.container}>
         
             <Modal
                 animationType="fade"
@@ -118,43 +162,60 @@ export function component_userDetails({route,navigation})
                     setModalVisible(!modalVisible);
                 }}>
 
-                <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-                    <View style={{backgroundColor:'#ffffff',height:200,width:300,borderRadius:25,justifyContent:'center',alignItems:'center'}}>
+                <View style={styles.modalandpressable}>
+                    <View style={styles.modalinside}>
                         <Pressable
-                            style={{flex:1,justifyContent:'center',alignItems:'center'}}
+                            style={styles.modalandpressable}
                             onPress={() => function_openGallery()}>
-                            <Text style={{fontSize:22,fontFamily:'Montserrat-Regular'}}>Choose from gallery</Text>
+                            <Text style={styles.modaltext}>Choose from gallery</Text>
                         </Pressable>
                         <Pressable
-                            style={{flex:1,justifyContent:'center',alignItems:'center'}}
+                            style={styles.modalandpressable}
                             onPress={() => function_openCamera()}>
-                            <Text style={{fontSize:22,fontFamily:'Montserrat-Regular'}}>Take from camera</Text>
+                            <Text style={styles.modaltext}>Take from camera</Text>
                         </Pressable>
                         <Pressable
-                            style={{flex:1,justifyContent:'center',alignItems:'center'}}
+                            style={styles.modalandpressable}
                             onPress={() => setModalVisible(!modalVisible)}>
-                            <Text style={{fontSize:22,fontFamily:'Montserrat-SemiBold'}}>Cancel</Text>
+                            <Text style={[styles.modaltext,{fontFamily:'Montserrat-SemiBold'}]}>Cancel</Text>
                         </Pressable>
                     </View>
                 </View>
             </Modal>
 
-            <View style={{justifyContent:'center',alignItems:'center',height:250}}>
-                <View style={{height:160,width:160,backgroundColor:'#3AF9EFFF',justifyContent:'center',alignItems:'center',borderRadius:50,transform:[{rotate:'-57deg'}]}}>
-                    <View style={{height:160,width:160,backgroundColor:'#C72FF8FF',borderRadius:50,justifyContent:'center',alignItems:'center',transform:[{rotate:'27deg'}]}}>
-                        <Pressable onPress={()=>setModalVisible(true)} style={{height:150,width:150,backgroundColor:'#ffffff',borderRadius:50,justifyContent:'center',alignItems:'center',transform:[{rotate:'30deg'}]}}>
-                            <Icon name="camera-outline" size={width/10} color='#3E4DC8'/>
-                        </Pressable>
+            {
+                userProfileName=='defaultProfile.png'?
+                    <View style={styles.upperView}>
+                        <View style={styles.shapesWithOut}>
+                            <View style={styles.shapesWithIn}>
+                                <Pressable onPress={()=>setModalVisible(true)} style={[styles.profilePressable,{transform:[{rotate:'30deg'}]}]}>
+                                    <Icon name="camera-outline" size={width/10} color='#3E4DC8'/>    
+                                </Pressable>
+                            </View>
+                        </View>
                     </View>
-                </View>
-            </View>
-            
-            <View style={{flex:1,marginHorizontal:24,marginVertical:8}}>
+                    :
+                    <View style={styles.upperView}>
+                        <View style={styles.shapesWithoutOut}>
+                            <View style={styles.shapesWithoutIn}>
+                            </View>
+                        </View>
+                        <View>
+                            <Pressable onPress={()=>setModalVisible(true)} style={styles.profilePressable}>
+                                <Image borderRadius={50} source={{width:height/4.5,height:height/4.5,borderRadius:height/16,uri:userProfile}}/>    
+                            </Pressable>     
+                        </View>
+                    </View>
+            }
 
-                <View style={{marginVertical:18}}>
-                    <Text style={{color:'#ffffff',fontSize:width/26,fontFamily:'Montserrat-Regular'}}>User Name</Text>
+            
+
+            <View style={styles.midView}>
+
+                <View style={styles.textView}>
+                    <Text style={styles.userheading}>User Name</Text>
                     <TextInput
-                        style={{color:'#ffffff',marginVertical:4,fontSize:width/22,borderBottomWidth:1,borderColor:"#ffffff",letterSpacing:3,fontFamily:'Montserrat-Medium'}}
+                        style={styles.textInput}
                         placeholderTextColor="#b9b9b9"
                         placeholder="Your username"
                         value={userName}
@@ -162,29 +223,39 @@ export function component_userDetails({route,navigation})
                     />
                 </View>
 
-                <View style={{marginVertical:18}}>
-                    <Text style={{color:'#ffffff',fontSize:width/26,fontFamily:'Montserrat-Regular'}}>User Impaired</Text>
-                    <View>
-                        <Picker 
-                            selectedValue={selectedOption}
-                            mode='dropdown'
-                            style={{color:'#ffffff',marginVertical:4}}
-                            onValueChange={(itemValue, itemIndex) =>
-                                console.log(itemValue)
-                            }>
-                            <Picker.Item label="No" value="notImpaired" style={{fontSize:width/24}} />
-                            <Picker.Item label="Yes" value="Impaired" style={{fontSize:width/24}} />
-                        </Picker>
-                        <View style={{backgroundColor:'#ffffff',height:1}}></View>
+                {
+                    !keyboardStatus?
+                    <View style={styles.impairedView}>
+                        <Text style={styles.imapairedHead}>User Impaired</Text>
+                        <View>
+                            <Picker 
+                                selectedValue={selectedOption}
+                                mode='dropdown'
+                                style={styles.pickerView}
+                                onValueChange={(itemValue, itemIndex) =>
+                                    set_userState(itemValue)
+                                }>
+                                <Picker.Item label="No" value="notImpaired" style={styles.pickerItem} />
+                                <Picker.Item label="Yes" value="Impaired" style={styles.pickerItem} />
+                            </Picker>
+                            <View style={styles.pickerLine}></View>
+                        </View>
                     </View>
-                </View>
-                
-                <TouchableOpacity style={styles.touchableopacity} onPress={function_submitDetails}>
-                    <View style={styles.touchableopacityView}>
-                        <Text style={styles.submitDetails}>Complete</Text>
-                        <Icon name="checkmark" size={width/18} color='#3E4DC8'/>
-                    </View>
-                </TouchableOpacity>
+                    :
+                    <View></View>
+                }
+
+                {
+                    !keyboardStatus?           
+                    <TouchableOpacity style={styles.touchableopacity} onPress={function_submitDetails}>
+                        <View style={styles.touchableopacityView}>
+                            <Text style={styles.submitDetails}>Complete</Text>
+                            <Icon name="checkmark" size={width/18} color='#3E4DC8'/>
+                        </View>
+                    </TouchableOpacity>
+                    :
+                    <View></View>
+                }
             </View>
 
             <View style={styles.lineView}>
