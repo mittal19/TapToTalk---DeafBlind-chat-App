79ba4caf1bab8af374a5887814173bd5bb7df2de
Priyanshu Mittal <mittalpriyanshu19@gmail.com>
2021-04-23 20:00:35 +0530

    added backgorund to activity indicator

--- components/component_userNumber.js
+++ components/component_userNumber.js
@@ -94,8 +94,8 @@ export function component_userNumber({navigation})
   if(sendingOtp==true)           //if otp is not sent then show activity indicator 
   {
     return(
-      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-        <ActivityIndicator size="large" color="#000000"/>
+      <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'#ffffff'}}>
+        <ActivityIndicator size="large" color="#3E4DC8"/>
       </View>
     );
   }
