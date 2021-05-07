147afeb2abe2c44d96165171dcff58e381d22a9f
Priyanshu Mittal <mittalpriyanshu19@gmail.com>
2021-05-07 20:55:41 +0530

    added code for retriving lastest/recent messages

--- components/component_home.js
+++ components/component_home.js
@@ -9,7 +9,8 @@ GLOBAL = require('../global');
 export function component_home({navigation})
 {
   const {logOut} = React.useContext(AuthContext);          //accessing auth context function logOut created at App.js file
-  
+
+  const [recentMessages,set_recentMessages] = React.useState([]);
   const [activityIndicator,set_activityIndicator] = React.useState(true);   // this usestate will decide whether to show activity indicator or not.
 
   const checkcontacts = async()=>     //this function will be called when userr click on contacts
@@ -20,15 +21,35 @@ export function component_home({navigation})
   useEffect(async()=>  //this will be automattically called is similar to component did mount
   { 
     const firestoreRef = firestore()
-                            .collection(GLOBAL.userNumber);
-
-    const subscribe = firestoreRef.onSnapshot(querySnapshot => {
+                            .collection(GLOBAL.userNumber).orderBy('createdAt',"desc");
+    
+    firestoreRef.onSnapshot(snapshot=>(
+      set_recentMessages(snapshot.docs.map(doc=>
+        ({
+          id:doc.id,
+          createdAt:doc.data().createdAt,
+          text:doc.data().text
+        })
+        ))
+    ))
+  
+    /*const subscribe = firestoreRef.onSnapshot(querySnapshot => {
       querySnapshot.docs.map(documentSnapshot=>
         {
-          console.log(documentSnapshot.data())
+          console.log(documentSnapshot.id);
+          console.log(documentSnapshot.data().createdAt);
+          console.log(documentSnapshot.data().text);
+          temp_recentMessages.push({
+            id:documentSnapshot.id,
+            createdAt:documentSnapshot.data().createdAt,
+            text:documentSnapshot.data().text
+          })
         })
+      console.log(temp_recentMessages);
+      if(isMounted)
+        set_recentMessages(temp_recentMessages);
     });
-    
+    */
     set_activityIndicator(false);
     
   },[]);
@@ -46,6 +67,13 @@ export function component_home({navigation})
     <View style={{flex:1,justifyContent:'space-between'}}>
       <Text>HOME</Text>
       <Text>{GLOBAL.userNumber}</Text>
+      <View>
+      {
+        recentMessages.map((item,key)=>(
+          <Text key={key}>{item.text}</Text>
+        ))
+      }
+      </View>
       <TouchableOpacity onPress={checkcontacts} ><Text>CONTACTS</Text></TouchableOpacity>
       <TouchableOpacity onPress={logOut}><Text>Logout</Text></TouchableOpacity>
     </View>
