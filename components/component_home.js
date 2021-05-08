11a815d5d80a3d89edb74011225514efff9aeeb8
Priyanshu Mittal <mittalpriyanshu19@gmail.com>
2021-05-08 22:11:06 +0530

    adding front end

--- components/component_home.js
+++ components/component_home.js
@@ -1,10 +1,12 @@
 //here for now user can logout or see contacts 
 
 import React,{useState,useEffect} from 'react';
-import {View,Text,TouchableOpacity,ActivityIndicator} from 'react-native';
+import {View,Text,TouchableOpacity,ActivityIndicator,ScrollView,Image,Dimensions} from 'react-native';
 import {AuthContext} from '../helpers/context';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import firestore from '@react-native-firebase/firestore';
+import Icon from 'react-native-vector-icons/Ionicons';
+const {width, height} = Dimensions.get('window');
+
 GLOBAL = require('../global');
 export function component_home({navigation})
 {
@@ -13,7 +15,7 @@ export function component_home({navigation})
   const [recentMessages,set_recentMessages] = React.useState([]);
   const [activityIndicator,set_activityIndicator] = React.useState(true);   // this usestate will decide whether to show activity indicator or not.
 
-  const checkcontacts = async()=>     //this function will be called when userr click on contacts
+  const function_checkcontacts = async()=>     //this function will be called when userr click on contacts
   {       
     navigation.navigate('Contacts');  //navigating to contacts component and passing userNumber to next screen
   }
@@ -26,30 +28,15 @@ export function component_home({navigation})
     firestoreRef.onSnapshot(snapshot=>(
       set_recentMessages(snapshot.docs.map(doc=>
         ({
-          id:doc.id,
+          id:doc.data().id,
+          sender:doc.id,
           createdAt:doc.data().createdAt,
-          text:doc.data().text
+          text:doc.data().text,
+          senderProfile:doc.data().receiverProfile
         })
         ))
     ))
   
-    /*const subscribe = firestoreRef.onSnapshot(querySnapshot => {
-      querySnapshot.docs.map(documentSnapshot=>
-        {
-          console.log(documentSnapshot.id);
-          console.log(documentSnapshot.data().createdAt);
-          console.log(documentSnapshot.data().text);
-          temp_recentMessages.push({
-            id:documentSnapshot.id,
-            createdAt:documentSnapshot.data().createdAt,
-            text:documentSnapshot.data().text
-          })
-        })
-      console.log(temp_recentMessages);
-      if(isMounted)
-        set_recentMessages(temp_recentMessages);
-    });
-    */
     set_activityIndicator(false);
     
   },[]);
@@ -64,18 +51,54 @@ export function component_home({navigation})
   }
 
   return(
-    <View style={{flex:1,justifyContent:'space-between'}}>
-      <Text>HOME</Text>
-      <Text>{GLOBAL.userNumber}</Text>
-      <View>
-      {
-        recentMessages.map((item,key)=>(
-          <Text key={key}>{item.text}</Text>
-        ))
-      }
+    <View style={{flex:1,backgroundColor:'#3E4DC8'}}>
+
+      <View style={{height:56,justifyContent:'space-between',alignItems:'center',flexDirection:'row',marginHorizontal:20}}>
+        
+        <View style={{backgroundColor:'#4f5fe3',height:40,width:40,justifyContent:'center',alignItems:'center',borderRadius:8}}>
+          <Icon name="reorder-three-outline" size={32} color="#ffffff"></Icon>
+        </View>
+        
+        <Text style={{color:'#ffffff',fontSize:24,fontFamily:'Montserrat-Regular'}}>Messages</Text>
+        
+        <View style={{backgroundColor:'#4f5fe3',height:40,width:40,justifyContent:'center',alignItems:'center',borderRadius:8}}>
+          <Icon name="search-outline" size={24} color="#ffffff"></Icon>
+        </View>
+
       </View>
-      <TouchableOpacity onPress={checkcontacts} ><Text>CONTACTS</Text></TouchableOpacity>
-      <TouchableOpacity onPress={logOut}><Text>Logout</Text></TouchableOpacity>
+
+      <View style={{flex:1,backgroundColor:'#ffffff',borderTopLeftRadius:48,borderTopRightRadius:48}}>
+        
+        <ScrollView style={{marginHorizontal:20,marginVertical:16}}>
+          {
+            recentMessages.map((item,key)=>(
+              
+              <View key={item.id} style={{flexDirection:'row',alignItems:'center',marginVertical:8,borderWidth:2,width:width-40,borderColor:'#3E4DC8',padding:8,borderRadius:16}}>
+                <Image
+                  source={{ uri: item.senderProfile }}
+                  style={{width:48,height:48,borderRadius:44}}
+                  PlaceholderContent={<Text style={{fontSize:8}}>Image</Text>}
+                />
+                <View style={{marginHorizontal:16,flex:1}}>
+                  <View style={{flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+                    <Text numberOfLines={1} style={{marginBottom:4,fontSize:18,fontFamily:'Montserrat-Medium'}}>{item.sender}</Text>
+                    <Text style={{marginBottom:4,fontSize:12}}>{item.createdAt.hour}:{item.createdAt.minute}</Text>
+                  </View>
+                  <Text numberOfLines={1} style={{fontSize:16}}>{item.text}</Text>
+                </View>
+              </View>
+            ))
+          }
+        </ScrollView>
+
+        <TouchableOpacity 
+          onPress={function_checkcontacts}
+          style={{position:'absolute',right:28,bottom:16,backgroundColor:'#3E4DC8',height:44,width:44,justifyContent:'center',alignItems:'center',borderRadius:24}}>
+          <Icon name="add" size={34} color="#ffffff"></Icon>
+        </TouchableOpacity>
+
+      </View>
+
     </View>
   );
 
