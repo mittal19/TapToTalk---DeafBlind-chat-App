6bfdddc7f38949fac99c0646a202fb7edf1beead
Priyanshu Mittal <mittalpriyanshu19@gmail.com>
2021-05-10 22:05:17 +0530

    Changed the code for seeing whether number is on taptotalk or not

--- components/component_contacts.js
+++ components/component_contacts.js
@@ -17,128 +17,121 @@ const {width, height} = Dimensions.get('window');
 
 export function component_contacts({navigation})
 {
-
   const sender = GLOBAL.userNumber;
   
   const dbref = firebase.database().ref();     //setting reference to real time database
-  const [isLoading,set_isLoading] = useState(true);
+  
+  const [activityIndicator,set_activityIndicator] = useState(true);
   const [onTapToTalk,set_onTapToTalk] = useState([]);   //hold contacts on taptotalk
-  const [notTapToTalk,set_notTapToTalk] = useState([]);   //hold contacts not on taptotalk
   const [modalVisible, set_modalVisible] = useState(false);
   const [openImg,set_openImg] = useState();
+  const [showProfile,set_showProfile] = useState(false);
 
-  useEffect(()=>
+  useEffect(async()=>
   {
-      async function functionname()   //creating a function with name of 'functionname' 
+    var raw_Contacts=[];     //hold all phone contacts
+
+    if(Platform.OS == 'android')
+    {
+      try
       {
-        var contacts=[];     //hold all phone contacts
+        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS);
 
-        if(Platform.OS == 'android')
+        if(granted === PermissionsAndroid.RESULTS.GRANTED)
         {
-          try
-          {
-            const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS);
-
-            if(granted === PermissionsAndroid.RESULTS.GRANTED)
+          await Contacts.getAll().then(contacts =>   //getting all phone contacts
             {
-              await Contacts.getAll().then(cont =>   //getting all phone contacts
-                {
-                  contacts=cont;    
-                });
-              console.log("in1");
+              raw_Contacts=contacts;    
+            });
 
-              const query = await dbref.child('users');   //refering to users in realtime database
-              const users = await query.once('value')    //users variable will have details of all users on taptotalk
-                                    .then(
-                                      function(snap)
-                                      {
-                                        return snap.val();
-                                      }
-                                    );
-              console.log("in2");
-              var on=[];    //temporary variable will hold users on taptotalk
-              var not=[];   ////temporary variable will hold users not on taptotalk
-              var x=0;    //index for on variable
-              var y=0;   //index for not variable
-              for(var i=0;i<contacts.length;i++)
+          var formated_Contacts=[];
+
+          const usersOnApp = await dbref.child('users').once('value').then(snap=>{return snap.val()});
+          
+          var nonDuplicatePhoneNumbers={};
+
+          for(var i=0;i<raw_Contacts.length;i++)
+          {
+            if(raw_Contacts[i].phoneNumbers.length!=0) 
+            {
+              var phoneNumber = raw_Contacts[i].phoneNumbers[0].number;
+              phoneNumber = phoneNumber.replace(/\D/g,'').slice(-10);
+            
+              if(nonDuplicatePhoneNumbers[phoneNumber]==undefined)
               {
-                if(contacts[i].phoneNumbers.length!=0)    //if phone contact have no number
+                nonDuplicatePhoneNumbers[phoneNumber]=1;
+              
+                var onTapToTalk = "No";
+                var userProfile = "";
+                
+                if(usersOnApp[phoneNumber]!=undefined)
                 {
-
-                  var tempphonenumber = contacts[i].phoneNumbers[0].number;
-                  tempphonenumber = tempphonenumber.replace(/\D/g,'').slice(-10);
-
-                  if(users[tempphonenumber]!=undefined)    //if phone contact is present on tap to talk
-                  {
-                    var profilename = users[tempphonenumber].userProfile;
-                    var profileLink= await storage().ref('defaultProfile.png').getDownloadURL();
-                    if(profilename!='defaultProfile.png')
-                    {
-                      profileLink = await storage().ref(profilename).getDownloadURL();  
-                    }
-
-                    var enrty =    //creating a entry for 'on' array
-                    {
-                      id:x,   //index
-                      userName:contacts[i].displayName,
-                      userNumber:tempphonenumber,
-                      userProfile:profileLink,
-                    };
-                    x++;
-                    on.push(enrty);
-                  }
-                  else
-                  {    ///contacs of phone not on taptotalk
-                    var profileLink = 'https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png';
-                    var enrty =    //creating entty for 'not' array
-                    {
-                      id:y,    //index
-                      userName:contacts[i].displayName,
-                      userNumber:tempphonenumber,
-                      userProfile:'',
-                    };
-                    y++;
-                    not.push(enrty);
-                  } 
+                  onTapToTalk = "Yes";
+                  userProfile = usersOnApp[phoneNumber].userProfile;
                 }
+                
+                formated_Contacts.push(
+                  {
+                    userNumber:phoneNumber,
+                    userName:raw_Contacts[i].displayName,
+                    onTapToTalk:onTapToTalk,
+                    userProfile:userProfile
+                  }
+                );
               }
-
-              console.log("in3");
-
-              on.sort(function(a,b)   //sorting 'on' according to their first name
-              {
-                return a.userName.toLowerCase()>b.userName.toLowerCase();
-              });
-              not.sort(function(a,b)   //sorting 'not' acc. to first name
-              {
-                return a.userName.toLowerCase()>b.userName.toLowerCase();
-              });
-
-              console.log("in4");
-
-              set_onTapToTalk(on);   //setting usestate
-              set_notTapToTalk(not);  //setting usestate
-              set_isLoading(false);  
-            }
-            else
-            {
-              console.log("permission denied"); 
-              navigation.goBack();   //permission denied to access contacts // going one screen back
-              ToastAndroid.show("Permission to access contacts denied",ToastAndroid.LONG);  
             }
           }
-          catch(err)
+          
+          formated_Contacts.sort(function(a,b)   //sorting 'on' according to their name
           {
-            console.log(err);
-            navigation.goBack();    //some error occurred going one screen back
-            ToastAndroid.show("Permission to access contacts denied",ToastAndroid.LONG);
+            return a.userName.toLowerCase()>b.userName.toLowerCase();
+          });
+
+          set_onTapToTalk(formated_Contacts);
+          
+          set_activityIndicator(false);
+
+          var default_ProfileURL = await storage().ref('defaultProfile.png').getDownloadURL();
+          
+          for(var i=0;i<formated_Contacts.length;i++)
+          {
+            if(formated_Contacts[i].userProfile!="")
+            {
+              
+              if(formated_Contacts[i].userProfile!="defaultProfile.png")
+              { 
+                var ProfileURL = await storage().ref(formated_Contacts[i].userProfile).getDownloadURL(); 
+                formated_Contacts[i].userProfile = ProfileURL;
+              }
+              else
+              {
+                formated_Contacts[i].userProfile = default_ProfileURL;
+              }
+            }
           }
+
+          set_onTapToTalk(formated_Contacts);
+          set_showProfile(true);
+        }
+        else
+        {
+          console.log("permission denied"); 
+          
+          navigation.goBack();   //permission denied to access contacts // going one screen back
+          ToastAndroid.show("Permission to access contacts denied",ToastAndroid.SHORT);  
         }
       }
-      functionname();    //calling above function
-    },[]);
+      catch(err)
+      {
+        console.log(err);
+
+        navigation.goBack();    //some error occurred going one screen back
+        ToastAndroid.show("Permission to access contacts denied",ToastAndroid.SHORT);
+      }
+    }
+  },[]);
   
-  const openpersonalmessage = (receiver)=>{            //this function will be called when user clicks on specific contact to begin chatting
+  const function_openpersonalmessage = (receiver)=>{            //this function will be called when user clicks on specific contact to begin chatting
     if(receiver.onTapToTalk=="false")       //checking if the contact user clicked on is on taptotalk or not
     {
       ToastAndroid.show("Not on TapToTalk! Invite them here.",ToastAndroid.LONG);        //if not then show this
@@ -150,13 +143,6 @@ export function component_contacts({navigation})
     }
   }
 
-  const invitethem = (receiver)=>
-  {
-    //showing toast when clicked on user who is not on taptotalk
-    console.log(receiver);
-    ToastAndroid.show(receiver.userName +" is not on TapToTalk! Invite them here.",ToastAndroid.SHORT);
-  }
-
   const function_openImg = (userProfile)=>
   {
     if(userProfile!='https://firebasestorage.googleapis.com/v0/b/taptotalk-ce0f0.appspot.com/o/defaultProfile.png?alt=media&token=7c559b92-a6a2-4ba7-ace9-9cbb3a8d6d2c')
@@ -166,17 +152,17 @@ export function component_contacts({navigation})
     }
   }
 
-  if(isLoading==true)
-  {  //showin loader  till contacts is not fetched
+  if(activityIndicator)
+  {  
     return(
-      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-        <ActivityIndicator size="large" color="#3E4DC8"/>
+      <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'#3E4DC8'}}>
+        <ActivityIndicator size="large" color="#FFFFFF"/>
       </View>
     );
   }
  
   return( 
-    <ScrollView style={{backgroundColor:'#3E4DC8'}}>
+    <View style={{flex:1,backgroundColor:'#3E4DC8'}}>
 
       <Modal
           animationType="fade"
@@ -196,35 +182,48 @@ export function component_contacts({navigation})
           </SafeAreaView>
       </Modal>
 
-      <SafeAreaView style={{marginHorizontal:16}}>
+    
+    <View style={{marginHorizontal:16}}>
+      
+      <Text style={{color:'#DCDCDC',fontSize:18,fontFamily:'Montserrat-Regular',marginTop:16}}>Contacts</Text>
+      
+      <ScrollView style={{ marginVertical:16}}>
+        {
+          onTapToTalk.map((item,key)=>(
+            <View key={item.userNumber} >
+              {
+                item.onTapToTalk=="Yes"?
+                <View style={{flexDirection:'row',marginVertical:6,flex:1,alignItems:'center'}}>
+                  <TouchableOpacity onPress={()=>function_openImg(item.userProfile)} style={{backgroundColor:'#ffffff',width:44,height:44,borderRadius:44}}>
+                    {
+                      showProfile?
+                      <Image
+                        source={{ uri: item.userProfile}}
+                        style={{width:44,height:44,borderRadius:44}}
+                        PlaceholderContent={<Text style={{fontSize:8}}>Image</Text>}
+                      />
+                      :
+                      <View style={{width:44,height:44,borderRadius:44,justifyContent:'center',alignItems:'center'}}>
+                        <ActivityIndicator size="small" color="#3E4DC8"/>
+                      </View>
+                    }
+                  </TouchableOpacity>
+                  
+                  <TouchableOpacity onPress={()=>function_openpersonalmessage(item)} style={{flex:1,justifyContent:'center',marginLeft:16,backgroundColor:'#ffffff',height:48,flex:1,borderRadius:8}}>
+                    <Text style={{marginLeft:12,fontSize:18,color:'#3E4DC8',fontFamily:'Montserrat-Medium'}}>{item.userName}</Text>
+                  </TouchableOpacity>
+                </View>
+                :
+                <View></View>
+              }
+            </View>
+          ))
+        }
+      </ScrollView>
 
-        <Text style={{color:'#DCDCDC',fontSize:18,fontFamily:'Montserrat-Regular',marginTop:16}}>Contacts</Text>
-
-        <FlatList    //this list will show users who are on taptotalk
-          style={{ marginVertical:16}}
-          data={onTapToTalk}
-          keyExtractor={({ id }, index) => id.toString()}
-          renderItem={({ item }) => (
-            <SafeAreaView style={{flexDirection:'row',marginVertical:6,flex:1,alignItems:'center'}}>
-
-              <TouchableOpacity onPress={()=>function_openImg(item.userProfile)} style={{backgroundColor:'#ffffff',width:44,height:44,borderRadius:44}}>
-                <Image
-                  source={{ uri: item.userProfile }}
-                  style={{width:44,height:44,borderRadius:44}}
-                  PlaceholderContent={<Text style={{fontSize:8}}>Image</Text>}
-                />
-              </TouchableOpacity>
-              
-              <TouchableOpacity onPress={()=>openpersonalmessage(item)} style={{flex:1,justifyContent:'center',marginLeft:16,backgroundColor:'#ffffff',height:48,flex:1,borderRadius:8}}>
-                <Text style={{marginLeft:12,fontSize:18,color:'#3E4DC8',fontFamily:'Montserrat-Medium'}}>{item.userName}</Text>
-              </TouchableOpacity>
-
-            </SafeAreaView>    
-          )} 
-        />
-      </SafeAreaView>
-
-    </ScrollView>
+    </View>
+       
+    </View>
   );
 }
 
