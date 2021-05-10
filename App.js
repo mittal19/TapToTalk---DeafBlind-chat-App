5fc79e222ebf6cab2ba8966b33b2bc0492af5c2a
Priyanshu Mittal <mittalpriyanshu19@gmail.com>
2021-05-10 14:18:16 +0530

    Added timeout function in login while storing data to firebase

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ Stack navigator for navigating between screens
 */
  
 import React,{useEffect} from 'react';
-import {View,ToastAndroid,Text,ActivityIndicator} from 'react-native';
+import {View,ToastAndroid,ActivityIndicator} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -32,6 +32,7 @@ const Stack = createStackNavigator();    //for creating navigation between scree
 GLOBAL = require('./global');  //make access of global usernumber here
 
 const debug = false;   //set to false if you dont want logs
+const timeout= 8000;
 
 function App()
 {
@@ -42,6 +43,78 @@ function App()
     const [loginState,dispatch] = React.useReducer(Reduceractions,initialLogin);   //intializing reducer , Reduceractions imported, initialLogin imported
 
     const [activityIndicator,set_activityIndicator] = React.useState(false);   // this usestate will decide whether to show activity indicator or not.
+
+    useEffect(async()=>  //this will be automattically called is similar to component did mount
+    {            
+      let userNumber = null;      //these variales will store data from local storage  
+      let userName = null;
+      let userState = null;
+      let userProfile = null;
+
+      try
+      {
+        debug && console.log("App.js - Getting userdetails from phone storage");
+
+        userNumber = await AsyncStorage.getItem('userNumber');      //getting phonenumber,name,profile,status from storage
+        userName = await AsyncStorage.getItem('userName');
+        userState = await AsyncStorage.getItem('userState');
+        userProfile = await AsyncStorage.getItem('userProfile');
+      }
+      catch(e)
+      {
+        ToastAndroid.show("Some error occurred while getting login information.",ToastAndroid.SHORT);
+        
+        debug && console.log("App.js - Error occured while Getting userdetails from phone storage");
+        debug && console.log(e);
+      }
+      
+      debug && console.log("App.js - got these details from phone storage");
+      debug && console.log(userNumber);
+      debug && console.log(userName);
+      debug && console.log(userState);
+      debug && console.log(userProfile);
+
+      debug && console.log("App.js - calling dispatch retieve function");
+
+      dispatch({type:'RETRIEVE_STORED_DATA',userNumber:userNumber,userName:userName,userState:userState,userProfile:userProfile});        //calling dispatcher action for setting the data retrived ... this action is in ./helpers/Reduceractions 
+      
+      debug && console.log("App.js - setting global variable");
+
+      GLOBAL.userNumber = userNumber;
+      
+      setTimeout(()=>
+      {
+        debug && console.log("App.js - hiding slpash screen");
+
+        SplashScreen.hide();
+      },800);
+
+    },[]);
+
+    const function_loginSetTimeOut = async(userdetailed)=>  //this function will be called to save usedetails in firebase real time db it has time out feature
+    {
+        debug && console.log("components_App.js - called login function with timeout");
+
+        let temp_timeout = timeout || 20000;   //if timeout is passed with option then set timeout to that else set timeout to 20000
+        let timeout_err =  
+        {
+            requestId:'',
+            response:'Timeout',
+            status:408
+        };//setting error object to display if time out occurs
+
+        debug && console.log("App.js - creating reference to real time storage");
+
+        var database = firebase.database(); 
+
+        debug && console.log("App.js - storing userdetails to storage");  
+
+        return new Promise(function(resolve,reject) //The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+        {
+            database.ref().update(userdetailed).then(resolve,reject);  
+            setTimeout(reject.bind(null,timeout_err),temp_timeout); //settimeout will run reject.bind after timeout seconds
+        });
+    }
 
     const authContext = React.useMemo(()=>      //creating authcontext .. the functions created here will be accessible all in the app
     ({        
@@ -65,14 +138,12 @@ function App()
             
             debug && console.log(userdetailed);
             
-            debug && console.log("App.js - creating reference to real time storage");
+            debug && console.log("App.js - calling login time out function");
 
-            var database = firebase.database();   
+            var resp = await function_loginSetTimeOut(userdetailed);
+            
+            debug && console.log(resp);
 
-            debug && console.log("App.js - storing userdetails to storage");
-            
-            await database.ref().update(userdetailed);   //details stored  
-            
             debug && console.log("App.js - storing userdetails to local storage");
 
             await AsyncStorage.setItem('userNumber',userNumber);            //set all details in local storage of phone
@@ -90,7 +161,7 @@ function App()
           }
           catch(e)
           {
-            ToastAndroid.show("Some error occurred! Try again",ToastAndroid.SHORT);
+            ToastAndroid.show("Check your internet connection or Try again.",ToastAndroid.SHORT);
             
             debug && console.log("App.js - some error occured while logging userin");
             debug && console.log(e);
@@ -140,53 +211,6 @@ function App()
         },
 
     }),[]);
-
-    useEffect(async()=>  //this will be automattically called is similar to component did mount
-    {            
-      let userNumber = null;      //these variales will store data from local storage  
-      let userName = null;
-      let userState = null;
-      let userProfile = null;
-
-      try
-      {
-        debug && console.log("App.js - Getting userdetails from phone storage");
-
-        userNumber = await AsyncStorage.getItem('userNumber');      //getting phonenumber,name,profile,status from storage
-        userName = await AsyncStorage.getItem('userName');
-        userState = await AsyncStorage.getItem('userState');
-        userProfile = await AsyncStorage.getItem('userProfile');
-      }
-      catch(e)
-      {
-        ToastAndroid.show("Some error occurred while getting login information.",ToastAndroid.SHORT);
-        
-        debug && console.log("App.js - Error occured while Getting userdetails from phone storage");
-        debug && console.log(e);
-      }
-      
-      debug && console.log("App.js - got these details from phone storage");
-      debug && console.log(userNumber);
-      debug && console.log(userName);
-      debug && console.log(userState);
-      debug && console.log(userProfile);
-
-      debug && console.log("App.js - calling dispatch retieve function");
-
-      dispatch({type:'RETRIEVE_STORED_DATA',userNumber:userNumber,userName:userName,userState:userState,userProfile:userProfile});        //calling dispatcher action for setting the data retrived ... this action is in ./helpers/Reduceractions 
-      
-      debug && console.log("App.js - setting global variable");
-
-      GLOBAL.userNumber = userNumber;
-      
-      setTimeout(()=>
-      {
-        debug && console.log("App.js - hiding slpash screen");
-
-        SplashScreen.hide();
-      },800);
-
-    },[]);
 
   
   if(activityIndicator==true)
